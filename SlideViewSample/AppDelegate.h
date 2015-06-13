3ee07eaa8f8a10621652541d1304f766837f1279
SUZUKIyuka <suzukiyuka1218@gmail.com>
2015-06-13 11:04:29 +0900

    Initial Commit

---
+++ SlideViewSample/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SlideViewSample
+//
+//  Created by nagata on 6/13/15.
+//  Copyright (c) 2015 nagata. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ SlideViewSample/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SlideViewSample
+//
+//  Created by nagata on 6/13/15.
+//  Copyright (c) 2015 nagata. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
